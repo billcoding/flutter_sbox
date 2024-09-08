b174dc00801b6400e5130260eb2bf14eaff88e62
billcoding <wangmiao07@sina.com>
2024-09-08 09:59:12 +0800

    feat: updates

--- ios/Classes/FlutterSboxPlugin.swift
+++ ios/Classes/FlutterSboxPlugin.swift
@@ -3,15 +3,19 @@ import UIKit
 
 public class FlutterSboxPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_sbox", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "Sbox/FlutterSboxPlugin", binaryMessenger: registrar.messenger())
     let instance = FlutterSboxPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
+    case "start":
+      result(true)
+    case "stop":
+      result(true)
+    case "serviceStarted":
+      result(true)
     default:
       result(FlutterMethodNotImplemented)
     }
