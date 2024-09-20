f8906ccc91a0b29f2d5ff8fcd25f3e59796fd250
billcoding <wangmiao07@sina.com>
2024-09-20 11:12:13 +0800

    feat: revert ios

--- ios/Classes/FlutterSboxPlugin.swift
+++ ios/Classes/FlutterSboxPlugin.swift
@@ -1,58 +1,19 @@
-//
-//  FlutterSboxPlugin.swift
-//  flutter_sbox
-//
-//  Created by local on 2024/9/10.
-//
-
 import Flutter
 import UIKit
 
-public class FlutterSboxPlugin: NSObject, FlutterPlugin {
-    private static let TAG = "Sbox/FlutterSboxPlugin"
-    public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: TAG, binaryMessenger: registrar.messenger())
-        let instance = FlutterSboxPlugin()
-        registrar.addMethodCallDelegate(instance, channel: channel)
+public class MyplsPlugin: NSObject, FlutterPlugin {
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "flutter_sbox", binaryMessenger: registrar.messenger())
+    let instance = MyplsPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch call.method {
+    case "getPlatformVersion":
+      result("iOS " + UIDevice.current.systemVersion)
+    default:
+      result(FlutterMethodNotImplemented)
     }
-    
-    private func debug(str: String) {
-       // debugPrint("\(FlutterSboxPlugin.TAG) \(str)")
-    }
-    
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        @Sendable func mainResult(_ res: Any?) async {
-            await MainActor.run {
-                result(res)
-            }
-        }
-        debug(str: "onMethodCall start")
-        let method = call.method
-        let arguments = call.arguments
-        let argumentsString = arguments as? String
-        debug(str: "onMethodCall method: \(method) arguments: \(String(describing: arguments))")
-        switch method {
-            case "startService":
-                Sbox.start()
-                result(true)
-            case "stopService":
-                Sbox.stop()
-                result(true)
-            case "serviceStarted":
-                result(Sbox.serviceStarted())
-            case "setOptionJson":
-                if let optionJson = argumentsString {
-                    Sbox.setOptionJson(optionJson)
-                }
-                result(true)
-            case "setConfigJson":
-                if let configJson = argumentsString {
-                    Sbox.setConfigJson(configJson)
-                }
-                result(true)
-            default:
-                result(FlutterMethodNotImplemented)
-        }
-        debug(str: "onMethodCall end")
-    }
+  }
 }
